380d9740575b98404f2532af3abf40a99aa00711
091-shivani <117371884+091-shivani@users.noreply.github.com>
2024-03-23 00:46:42 +0530

    Add files via upload

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,108 +1,109 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const { body, validationResult } = require('express-validator');
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const nodemailer=require("nodemailer");
+const User = require("../models/User");
+const foodItem = require("../models/foodItem");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
-const jwtSecret = "MynameisShivaniGattani1$#"
+const jwtSecret = "MynameisShivaniGattani1$#";
 
-
-const sendVerifyMail=async(name,email,user_id)=>{
-  try{
-      const transporter=nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        port:'587',
-        requireTLS:true,
-        auth:{
-          user:'asame2829@gmail.com',
-          pass:'yjlu pmdh odqd ccvv'
-        }
-      });
-      const mailOptions={
-        from:'asame2829@gmail.com',
-        to:email,
-        subject:'For verification mail',
-        html:'<p>Hii '+name+' ,please click here to <a href="http://localhost:5000/api/verify?id='+user_id+'">Verify</a> your mail</p>'
+const sendVerifyMail = async (name, email, user_id) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: "587",
+      requireTLS: true,
+      auth: {
+        user: "asame2829@gmail.com",
+        pass: "yjlu pmdh odqd ccvv",
+      },
+    });
+    const mailOptions = {
+      from: "asame2829@gmail.com",
+      to: email,
+      subject: "For verification mail",
+      html:
+        "<p>Hii " +
+        name +
+        ' ,please click here to <a href="http://localhost:5000/api/verify?id=' +
+        user_id +
+        '">Verify</a> your mail</p>',
+    };
+    // console.log(email)
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        //  console.log("hiis")
+      } else {
+        console.log("Email has been sent:- ", info.response);
       }
-     // console.log(email)
-      transporter.sendMail(mailOptions,function(error,info){
-        if(error)
-        {
-           console.log(error);
-         //  console.log("hiis")
-        }
-        else
-        {
-          console.log("Email has been sent:- ",info.response);
-        }
-      })
+    });
+  } catch (error) {
+    console.log(error.message);
   }
-  catch(error)
-  {
-    console.log(error.message)
-  }
-}
+};
 
-const verifyMail = async(req,res)=>{
-  try{
-    const updateInfo = await User.updateOne({_id:req.query.id},{$set:{is_verified:"1"}});
+const verifyMail = async (req, res) => {
+  try {
+    const updateInfo = await User.updateOne(
+      { _id: req.query.id },
+      { $set: { is_verified: "1" } }
+    );
     console.log(updateInfo);
-    res.send("email verified. You can now login to your account")
-
-  }catch(error)
-  {
-    console.log(error.message)
+    res.send("email verified. You can now login to your account");
+  } catch (error) {
+    console.log(error.message);
   }
-}
+};
 
 router.post(
   "/createuser",
   [
-    body('email').isEmail(),
-    body('name').isLength({ min: 5 }),
-    body('password').isLength({ min: 5 }).withMessage('Invalid Password')
+    body("email").isEmail(),
+    body("name").isLength({ min: 5 }),
+    body("password").isLength({ min: 5 }).withMessage("Invalid Password"),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array(),message:"Invalid credentials" });
+      return res
+        .status(400)
+        .json({ errors: errors.array(), message: "Invalid credentials" });
     }
 
     const salt = await bcrypt.genSalt(10);
     let secPassword = await bcrypt.hash(req.body.password, salt);
-    
+
     try {
-      const userData=await User.create({
+      const userData = await User.create({
         name: req.body.name,
         password: secPassword,
         email: req.body.email,
-        location: req.body.location
+        location: req.body.location,
       });
-      if(req.body.email.endsWith("@nitt.edu"))
-         sendVerifyMail(userData.name,userData.email,userData._id);
-      else 
-         throw errors;
-      res.json({ success: true,message:"Verification email has been sent. Please verify your email" });
+      if (req.body.email.endsWith("@nitt.edu"))
+        sendVerifyMail(userData.name, userData.email, userData._id);
+      else throw errors;
+      res.json({
+        success: true,
+        message: "Verification email has been sent. Please verify your email",
+      });
     } catch (error) {
       console.log(error);
-      res.json({ success: false,message:"Invalid credentials" });
+      res.json({ success: false, message: "Invalid credentials" });
     }
   }
 );
 
-
-
-
 router.post(
   "/loginuser",
   [
-    body('email').isEmail(),
-    body('password').isLength({ min: 5 }).withMessage('Invalid Password')
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }).withMessage("Invalid Password"),
   ],
   async (req, res) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -110,29 +111,30 @@ router.post(
 
     let email = req.body.email;
     try {
-      let userData = await User.findOne({email});
-      if(!userData){
-        return res.status(400).json({ errors: "mail is incorrect"})
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res.status(400).json({ errors: "mail is incorrect" });
       }
 
-      const pwdCompare = await bcrypt.compare(req.body.password, userData.password)
-    
-      if(!pwdCompare){
-        return res.status(400).json({ errors: "password wrong"})
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+
+      if (!pwdCompare) {
+        return res.status(400).json({ errors: "password wrong" });
       }
-      
-       if(req.body.is_verified==="0")
-       {
-        return res.status(400).json({ errors: "email not verified"})
-       }
+
+      if (req.body.is_verified === "0") {
+        return res.status(400).json({ errors: "email not verified" });
+      }
       const data = {
-          user:{
-            id:userData.id
-          }
-      }
-      const authToken = jwt.sign(data, jwtSecret)
-      return res.json({ success: true, authToken: authToken})
-
+        user: {
+          id: userData.id,
+        },
+      };
+      const authToken = jwt.sign(data, jwtSecret);
+      return res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
@@ -140,6 +142,24 @@ router.post(
   }
 );
 
-router.get('/verify',verifyMail);
+router.post("/fooditem", async (req, res) => {
+  try {
+    const options = { half: req.body.half, full: req.body.full };
+    console.log(options);
+    const dishData = await foodItem.create({
+      CategoryName: req.body.CategoryName,
+      name: req.body.name,
+      img: req.body.img,
+      options: options,
+      email: req.body.email,
+      description: "Indian masala",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+router.get("/verify", verifyMail);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const { mongoDB } = require('./db');
-const { header } = require('express-validator');
+const { mongoDB } = require("./db");
+const { header } = require("express-validator");
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
 app.use(express.json());
-app.use('/api', require('./Routes/CreateUser'));
-app.use('/api', require('./Routes/DisplayData'));
-app.use('/api', require('./Routes/OrderData'));
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
+app.use("/api", require("./Routes/OrderData"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 mongoDB()
@@ -28,5 +28,5 @@ mongoDB()
     });
   })
   .catch((error) => {
-    console.log('Error connecting to MongoDB:', error);
+    console.log("Error connecting to MongoDB:", error);
   });
